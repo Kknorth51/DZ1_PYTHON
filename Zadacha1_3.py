303afb1138a0f000b99c510219d772336bf05c16
kknorth <kirillacid@gmail.com>
2023-02-19 17:42:02 +0700

    Требуется вывести все целые степени двойки  (т.е. числа вида 2k), не превосходящие числа N.

---
+++ Zadacha1_3.py
@@ -0,0 +1,21 @@
+# Задача №3:
+# Требуется вывести все целые степени двойки 
+# (т.е. числа вида 2k), не превосходящие числа N.
+
+# Пример:
+# 10 -> 1 2 4 8
+
+n = int(input('-->  '))
+print()
+
+twoDegree = 0
+
+poweredNumber = int()
+
+while(poweredNumber < n):
+    poweredNumber = 2 ** twoDegree
+    print(f'2^{twoDegree} = {poweredNumber}')
+    twoDegree += 1
+    poweredNumber = 2 ** twoDegree
+
+print()
